c65b0cfd0105df5e46b2417c22d96641fef3dda5
Tim <tim.langner0@gmail.com>
2020-11-13 11:09:46 +0100

    :construction: Add weather-card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import WeatherCard from './components/WeatherCard';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+      <div className="App">
+        {/* dt uses milliseconds but javascript uses milliseconds, multiply with 1000 */}
+        <WeatherCard
+            dt={1602104400 * 1000}
+            temp_min="22.67"
+            temp_max="24.39"
+            main="Clear"
+            icon="01d"
+        />
+      </div>
+  );
+};
 
 export default App;
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
+    // create a date object with Date class constructor
+    const date = new Date(dt);
+    return (
+        <Card style={{width: '18rem'}}>
+            <Card.Img
+                variant="top"
+                // get the src from example url and pass the icon prop for icon code
+                src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+            />
+            <Card.Body>
+                <Card.Title>{main}</Card.Title>
+                {/* turning datetime into local datetime */}
+                <p>
+                    {date.toLocaleDateString()} - {date.toLocaleTimeString()}
+                </p>
+                <p>Min: {temp_min}</p>
+                <p>Max: {temp_max}</p>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default WeatherCard;
