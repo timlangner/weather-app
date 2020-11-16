e6809d364b92480acd72d039b89d87f23719493d
Tim <tim.langner0@gmail.com>
2020-11-16 09:40:22 +0100

    :art: Clear previous search

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,29 +1,38 @@
 import {useState, useEffect} from 'react';
 
 const UseFetch = (initialUrl) => {
-
+    // create state variables
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
     const [url, setUrl] = useState(initialUrl);
 
     useEffect(() => {
+        if(!url) return;
+
         setIsLoading(true);
 
-        // check if we have an url for the initial render
-        if(!url) return;
+        // clear previous search
+        setData(null);
+        setError(null);
 
         fetch(url)
             .then((response) => response.json())
-                    .then((data) => {
-                        setIsLoading(false);
-                        setData(data);
-                    })
-                    .catch((error) => {
-                        setIsLoading(false);
-                        setError(error);
-                    });
-            }, [url]);
+            .then((data) => {
+
+                // error handling for nonexistent data
+                setIsLoading(false);
+                if(data.cod >= 400) {
+                    setError(data.message);
+                    return;
+                }
+                setData(data);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                setError(error);
+            });
+    }, [url]);
 
     return { data, error, isLoading, setUrl };
 };
